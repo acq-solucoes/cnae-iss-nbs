dfab3f3dbf9d6f658218cba2b4709c5bf5b158d1
acq-solucoes <acqcontabackup@gmail.com>
2026-03-08 11:42:03 -0300

    Força busca única e atualiza cache-busting de favicon/manifest

--- components/SearchBox.js
+++ components/SearchBox.js
@@ -1,3 +1,3 @@
 export function renderSearchBox() {
-  return `<section class="global-search"><label class="search-label" for="global-q">Busca global NCM/CNAE</label><div class="search-row"><input id="global-q" class="search-input" placeholder="Ex: 40117000, 6201500, máquinas agrícolas" autocomplete="off"/><button id="global-clear" class="btn">Limpar</button></div><div id="global-ac" class="ncm-autocomplete" style="display:none"></div></section>`;
+  return `<section class="global-search"><label class="search-label" for="global-q">Busca global NCM/CNAE</label><div class="search-row"><input id="global-q" class="search-input" placeholder="Ex: 40117000, 4520-0/01, máquinas agrícolas..." autocomplete="off"/><button id="global-clear" class="btn-clear">Limpar</button></div><div id="global-ac" class="ncm-autocomplete" style="display:none"></div></section>`;
 }
